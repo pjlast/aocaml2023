abd93b9b9ab14e07207a7ce015352de5349f030d
Petri-Johan Last <petri.last@sourcegraph.com>
2023-12-06 10:44:02 +0200

    Actually calculate part 2 from the original input numbers

--- bin/day06_main.ml
+++ bin/day06_main.ml
@@ -2,11 +2,10 @@ open Day06
 open Stdio
 
 let input = [ (54, 302); (94, 1476); (65, 1029); (92, 1404) ]
-let input2 = [ (54946592, 302147610291404) ]
 
 let () =
-    print_endline "day 6 part 1";
-    print_endline (Int.to_string (process_races input));
+  print_endline "day 6 part 1";
+  print_endline (Int.to_string (process_races input));
 
-    print_endline "day 6 part 2";
-    print_endline (Int.to_string (process_races input2))
+  print_endline "day 6 part 2";
+  print_endline (Int.to_string (process_races [ calc_input input ]))
--- lib/day06/day06.ml
+++ lib/day06/day06.ml
@@ -8,20 +8,21 @@ let calc_ways_to_win time dist =
       let d = (time - t) * t in
       if d > dist then Some t else None)
 
-let%test _ =
-    calc_ways_to_win 7 9 |> List.length = 4
-
-let%test _ =
-    calc_ways_to_win 15 40 |> List.length = 8
+let%test _ = calc_ways_to_win 7 9 |> List.length = 4
+let%test _ = calc_ways_to_win 15 40 |> List.length = 8
 
 let process_races races =
-    List.fold (List.map races ~f:(fun (time, dist) ->
-        calc_ways_to_win time dist |> List.length
-    )) ~init:1 ~f:( * )
+  List.fold
+    (List.map races ~f:(fun (time, dist) ->
+         calc_ways_to_win time dist |> List.length))
+    ~init:1 ~f:( * )
 
-let%test _ =
-    process_races input = 288
-
+let%test _ = process_races input = 288
 let input_2 = [ (71530, 940200) ]
 
-let%test _ = process_races input_2 = 71503
+let calc_input races =
+  List.fold races ~init:("", "") ~f:(fun (time', distance') (time, distance) ->
+      (time' ^ (time |> Int.to_string), distance' ^ (distance |> Int.to_string)))
+  |> fun (time, distance) -> (time |> Int.of_string, distance |> Int.of_string)
+
+let%test _ = process_races [ calc_input input ] = 71503
